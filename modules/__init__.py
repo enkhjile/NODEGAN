85672a15f0036817c21dddb515482ad08b2b15ea
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-07 17:47:18 +0000

    final

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,7 +6,7 @@ try:
     from torchdiffeq import odeint_adjoint as odeint
 except ImportError:
     from pip._internal import main as pip
-    pip(['install', 'git+https://github.com/rtqichen/torchdiffeq'])
+    pip(['install', '--user', 'git+https://github.com/rtqichen/torchdiffeq'])
     from torchdiffeq import odeint_adjoint as odeint
 
 
--- train.py
+++ train.py
@@ -56,10 +56,10 @@ def download_from_cloud(bucket, remote_path, local_path):
     blob.download_to_filename(local_path)
 
 
-def evaluate(model, update_step, opt, writer, bucket, engine):
+def evaluate(model, update_step, writer, bucket, engine):
     device = torch.device('cuda:0')
     model.eval()
-    eval_paths = [os.path.join(opt['eval_path'], v) for v in ['Set14', 'Set5']]
+    eval_paths = [os.path.join(args.eval_path, v) for v in ['Set14', 'Set5']]
     metrics_list = []
 
     for eval_path in eval_paths:
@@ -68,9 +68,9 @@ def evaluate(model, update_step, opt, writer, bucket, engine):
         LQ_path = os.path.join(eval_path, eval_name) + '_LQ.lmdb'
         LQ_r_path = os.path.join(eval_path, eval_name) + '_LQ_restored.lmdb'
 
-        eval_set = ValDataset(HQ_path, LQ_path, LQ_r_path)
-        eval_loader = DataLoader(eval_set, batch_size=1, shuffle=False,
-                                 pin_memory=True, num_workers=0)
+        eval_set = ValDataset(HQ_path, LQ_path, LQ_r_path, args.scale)
+        eval_loader = DataLoader(
+            eval_set, batch_size=1, shuffle=False, num_workers=4)
 
         psrn_rgb = 0.0
         psrn_y = 0.0
@@ -88,14 +88,14 @@ def evaluate(model, update_step, opt, writer, bucket, engine):
                 imgs = torch.cat([img_HQ, img_SR.detach().cpu(), img_LQ_r], dim=0)
                 grid = vutils.make_grid(imgs, nrow=3, normalize=False)
                 tmp_image = T.ToPILImage()(grid)
-                tmp_image.save('tmp_image.png')
-                upload_to_cloud(bucket, 'tmp_image.png',
-                               'odemsr/image_progress/gen_step_{}'.
-                               format(update_step * opt['update_freq']))
+                tmp_image.save('images/tmp_image.png')
+                upload_to_cloud(bucket, 'images/tmp_image.png',
+                                'odemsr/image_progress/gen_step_{}'.
+                                format(update_step * args.update_freq))
                 if eval_name == 'Set5':
                     writer.add_image('Set5', grid, update_step)
 
-            crop_size = opt['scale']
+            crop_size = args.scale
             img_HQ_rgb = img_HQ[0].permute(2, 1, 0).cpu(). \
                 numpy()[crop_size:-crop_size, crop_size:-crop_size, :]
             img_SR_rgb = img_SR[0].permute(2, 1, 0).detach().cpu(). \
@@ -122,9 +122,10 @@ def evaluate(model, update_step, opt, writer, bucket, engine):
             writer.add_scalar('ssim_y', ssim_y, update_step)
 
     query = '''
-        INSERT into odemsr_val (set14_psnr_rgb, set14_psnr_y, set14_ssim_rgb,
-        set14_ssim_y, set5_psnr_rgb, set5_psnr_y, set5_ssim_rgb,
-        set5_ssim_y) values (%f, %f, %f, %f, %f, %f, %f, %f)
+        INSERT INTO odemsr_val
+            (set14_psnr_rgb, set14_psnr_y, set14_ssim_rgb, set14_ssim_y,
+            set5_psnr_rgb, set5_psnr_y, set5_ssim_rgb, set5_ssim_y)
+        VALUES (%f, %f, %f, %f, %f, %f, %f, %f)
     ''' % tuple(metrics_list)
     engine.execute(query)
     model.train()
@@ -137,8 +138,7 @@ def train(resume=None):
     bucket = client.get_bucket('gloryofresearch')
     # connect to MySql
     engine = create_engine(
-        'mysql+pymysql://root:123@/model_monitoring?unix_socket=\
-        /cloudsql/helpful-quanta-248212:us-central1:model-monitoring-1')
+        'mysql+pymysql://root:123@/model_monitoring?unix_socket=/cloudsql/helpful-quanta-248212:us-central1:model-monitoring-1')
 
     print('Done')
     device = torch.device('cuda:0')
@@ -171,7 +171,7 @@ def train(resume=None):
         pin_memory=False)
     train_iterator = inf_generator(train_loader)
 
-    # start_time = time.time()
+    start_time = time.time()
     for train_step in range(start_step, args.train_steps + 1):
         s = time.time()
         netG.zero_grad()
@@ -196,33 +196,34 @@ def train(resume=None):
 
         schedulerG.step()
         print(time.time() - s)
-        # if train_step % opt['update_freq'] == 0:
-        #     elapsed_time = time.time() - start_time
-        #
-        #     state_dict = {
-        #         'netG': netG.state_dict(),
-        #         'optimG': optimG.state_dict(),
-        #         'schedulerG': schedulerG.state_dict(),
-        #         'gen_step': train_step
-        #     }
-        #     torch.save(state_dict, 'tmp_checkpoint.pth'.format(train_step))
-        #     upload_to_cloud(bucket, 'tmp_checkpoint.pth',
-        #                     'odemsr/model_checkpoints/gen_step_{}.pth'
-        #                     .format(train_step))
-        #
-        #     update_step = train_step // opt['update_freq']
-        #     writer.add_scalar('Loss', lossG.item(), update_step)
-        #     writer.add_scalar('NFE_F', nfe_f, update_step)
-        #     writer.add_scalar('NFE_B', nfe_b, update_step)
-        #
-        #     query = '''
-        #         INSERT into odemsr_train (time, loss, nfe_f, nfe_b)
-        #         VALUES ({}, {}, {}, {})
-        #         '''.format(elapsed_time, lossG.item(), nfe_f, nfe_b)
-        #     engine.execute(query)
-        #
-        #     evaluate(netG, update_step, opt, writer, bucket, engine)
-        #     start_time = time.time()
+        if train_step % args.update_freq == 0:
+            elapsed_time = time.time() - start_time
+            state_dict = {
+                'netG': netG.state_dict(),
+                'optimG': optimG.state_dict(),
+                'schedulerG': schedulerG.state_dict(),
+                'gen_step': train_step
+            }
+            torch.save(
+                state_dict, 'model_ckpt/tmp_checkpoint.pth'.format(train_step))
+            upload_to_cloud(bucket, 'model_ckpt/tmp_checkpoint.pth',
+                            'odemsr/model_checkpoints/gen_step_{}.pth'
+                            .format(train_step))
+
+            update_step = train_step // args.update_freq
+            writer.add_scalar('Loss', lossG.item(), update_step)
+            writer.add_scalar('NFE_F', nfe_f, update_step)
+            writer.add_scalar('NFE_B', nfe_b, update_step)
+
+            query = '''
+                INSERT INTO odemsr_train
+                    (time, loss, nfe_f, nfe_b)
+                VALUES ({}, {}, {}, {})
+                '''.format(elapsed_time, lossG.item(), nfe_f, nfe_b)
+            engine.execute(query)
+
+            evaluate(netG, update_step, writer, bucket, engine)
+            start_time = time.time()
 
 
 if __name__ == "__main__":
